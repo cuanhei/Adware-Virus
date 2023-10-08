d74963da5eb59c5b55dffb046ecd4e0c984a8031
Lim Cuan Hei <86009043+cuanhei@users.noreply.github.com>
2023-10-08 21:35:05 +0800

    Add files via upload

---
+++ virus/virus/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace virus
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 43);
+            label1.TabIndex = 0;
+            label1.Text = "Follow IG";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 28F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(352, 65);
+            label2.TabIndex = 1;
+            label2.Text = "@limcuanhei";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 21);
+            label3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(400, 200);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.Manual;
+            Text = "Virus";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            MouseClick += Form1_MouseClick;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ virus/virus/Form1.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace virus
+{
+    public partial class Form1 : Form
+    {
+        private const int WS_EX_TOOLWINDOW = 0x80;
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        public Form1()
+        {
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.TopMost = true;
+
+
+            InitializeComponent();
+            SetWindowLong(Handle, -20, GetWindowLong(Handle, -20) | WS_EX_TOOLWINDOW);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Check if Shift+A keys are pressed
+            if (e.KeyCode == Keys.Q)
+            {
+                // Close the form
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Follow IG @limcuanhei");
+            }
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Check if the left mouse button is clicked
+            if (e.Button == MouseButtons.Left)
+            {
+                // Show a message box
+                MessageBox.Show("Follow IG @limcuanhei");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
